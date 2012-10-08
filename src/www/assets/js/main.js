2bed71d3c7427452561a9f2d6c866d8b0a67652c
jkriddle <josh@theriddlebrothers.com>
2012-10-08 16:51:26 -0400

    Update project source  structure

---
+++ src/www/assets/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+	// Setup screenshot slideshow
+	$("#slides").slides({
+        preload: true,
+        preloadImage: 'assets/img/loading.gif',
+        play: 5000,
+        pause: 2500,
+        hoverPause: true
+      });
+})
